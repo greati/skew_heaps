1597eb32eadda42b85fe8be6086be668aaaa8f0d
Vitor Greati <greati@ufrn.edu.br>
2020-06-29 10:30:56 -0300

    Fix top down implementation

--- haskell/SkewHeap.hs
+++ haskell/SkewHeap.hs
@@ -8,7 +8,7 @@ import Heap
 import Data.Tree as DTree
 
 -- Top down implementation of skew heaps
-data SkewHeap a = Empty | Meld (SkewHeap a) a (SkewHeap a)
+data SkewHeap a = Empty | Tree (SkewHeap a) a (SkewHeap a)
     deriving (Eq)
 
 instance (Ord a, Show a, Eq a) => Show (SkewHeap a) where
@@ -18,31 +18,25 @@ instance (Ord a, Show a, Eq a) => Show (SkewHeap a) where
 instance (Ord a, Show a, Eq a) => Heap SkewHeap a where
     make_heap = Empty
 
-    make_singleton x = Meld Empty x Empty
+    make_singleton x = Tree Empty x Empty
 
     find_min Empty = Nothing
-    find_min (Meld _ x _ ) = Just x
+    find_min (Tree _ x _ ) = Just x
 
-    meld Empty  h2      = h2
-    meld h1      Empty  = h1
-    meld h1@(Meld _ x1 _) h2@(Meld Empty x2 Empty)
-        | x1 <= x2  = Meld h1 x1 Empty
-        | otherwise = Meld h1 x2 Empty 
-    meld h1@(Meld Empty x1 Empty) h2@(Meld _ x2 _)
-        | x1 <= x2  = Meld h2 x1 Empty
-        | otherwise = Meld h2 x2 Empty 
-    meld h1@(Meld l1 x1 r1) h2@(Meld l2 x2 r2) 
-        | x1 <= x2 = Meld (Meld (meld r1 r2) x2 l2) x1 l1
-        | otherwise = Meld (Meld (meld r1 r2) x1 l1) x2 l2
+    meld Empty  y       = y
+    meld x      Empty   = x
+    meld x@(Tree l1 a1 r1) y@(Tree l2 a2 r2) 
+        | a1 <= a2  = Tree (meld r1 y) a1 l1
+        | otherwise = Tree (meld r2 x) a2 l2
     
     insert x h = meld (make_singleton x) h 
 
     delete_min Empty = (Nothing, Empty)
-    delete_min (Meld l x r) = (Just x, meld l r)
+    delete_min (Tree l x r) = (Just x, meld l r)
 
-    toStringDataTree x@(Meld xl xa xr) = Node (show xa) [toStringDataTree xl, toStringDataTree xr]
+    toStringDataTree x@(Tree xl xa xr) = Node (show xa) [toStringDataTree xl, toStringDataTree xr]
     toStringDataTree Empty = Node "[]" []
 
---m1 = Meld (make_singleton 50) 1 (Meld (Meld Empty 13 (make_singleton 16)) 10 (Meld (make_singleton 25) 20 Empty))
---m2 = Meld (Meld (make_singleton 40) 19 Empty) 5 (Meld (make_singleton 30) 12 (make_singleton 14))
---m3 = Meld (Meld (Meld (Meld (Meld (Meld (make_singleton 25) 20 Empty) 14 Empty) 12 (make_singleton 30)) 10 (Meld Empty 13 (make_singleton 16))) 5 (Meld (make_singleton 40) 19 Empty)) 1 (make_singleton 50)
+m1 = Tree (make_singleton 50) 1 (Tree (Tree Empty 13 (make_singleton 16)) 10 (Tree (make_singleton 25) 20 Empty))
+m2 = Tree (Tree (make_singleton 40) 19 Empty) 5 (Tree (make_singleton 30) 12 (make_singleton 14))
+m3 = Tree (Tree (Tree (Tree (Tree (Tree (make_singleton 25) 20 Empty) 14 Empty) 12 (make_singleton 30)) 10 (Tree Empty 13 (make_singleton 16))) 5 (Tree (make_singleton 40) 19 Empty)) 1 (make_singleton 50)
